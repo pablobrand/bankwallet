9510b8ea37bd8d9b2b1137d99be3f3a6e3700c9c
harleydi <disneyharley26@gmail.com>
2023-10-14 13:55:49 -0400

    added files to get nfc-manager to work

--- bankwallet/App.js
+++ bankwallet/App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
 
 export default function App() {
+  const [hasNfc, setHasNFC] = useState(null);
+
+  useEffect(() => {
+    const checkIsSupported = async () => {
+      try {
+        const deviceIsSupported = await NfcManager.isSupported();
+        if (deviceIsSupported === null || deviceIsSupported === undefined) {
+          throw new Error('NFC support status is null or undefined');
+        }
+        
+        setHasNFC(deviceIsSupported);
+        if (deviceIsSupported) {
+          await NfcManager.start();
+        }
+      } catch (error) {
+        console.error('Error checking NFC support:', error);
+        // Handle the error as needed (e.g., show an error message to the user)
+      }
+    };
+
+    checkIsSupported();
+  }, []);
+
+  useEffect(() => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+      console.log('Tag found');
+    });
+
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    };
+  }, []);
+
+  const readTag = async () => {
+    try {
+      await NfcManager.registerTagEvent();
+    } catch (error) {
+      console.error('Error registering tag event:', error);
+      // Handle the error as needed (e.g., show an error message to the user)
+    }
+  };
+
+  const cancelReadTag = () => {
+    // Implement the cancel logic here
+    return NfcManager.cancelTechnologyRequest()
+  };
+
+  if (hasNfc === null) return null;
+
+  if (!hasNfc) {
+    return (
+      <View style={styles.sectionContainer}>
+        <Text>NFC not supported</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.sectionContainer}>
+      <Text>Hello world</Text>
+      <TouchableOpacity style={[styles.btn, styles.btnScan]} onPress={readTag}>
+        <Text style={{ color: 'white' }}>Scan Tag</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.btn, styles.btnCancel]} onPress={cancelReadTag}>
+        <Text style={{ color: 'white' }}>Cancel Scan</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  sectionContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    // Define your common button styles here
+  },
+  btnScan: {
+    // Define styles for the Scan button here
+  },
+  btnCancel: {
+    // Define styles for the Cancel button here
   },
 });
